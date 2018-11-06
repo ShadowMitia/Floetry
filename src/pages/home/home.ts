d0b195f126ec2644235b06730bdce31124abd4ef
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-11-06 17:21:59 +0100

    Reworked on button-color component

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { FeelsPage } from '../feels/feels'
+import { FeelsPage } from '../feels/feels';
 
 @Component({
   selector: 'page-home',
@@ -11,10 +11,10 @@ export class HomePage {
 
     feelings;
 
-	constructor(public navCtrl: NavController) 
+	constructor(public navCtrl: NavController)
 	  {
       this.feelings = [
-          {emotion:"happy", color:"#F7D26C"},
+        {emotion:"happy", color:"#F7D26C"},
           {emotion:"sad", color:"#67829F"},
           {emotion:"angry", color:"#A70032"},
           {emotion:"anxious", color:"#D25668"},
